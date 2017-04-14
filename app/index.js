8b2012178ea77e54a8ea6aebb8c6386e1ffbda0b
Neo <nihgwu@live.com>
2017-04-14 17:14:54 +0800

    add redux-persist

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
-import { AppRegistry } from 'react-native'
+import { AppRegistry, AsyncStorage } from 'react-native'
 import dva from 'dva/mobile'
+import { persistStore, autoRehydrate } from 'redux-persist'
 
 import { registerModels } from './models'
 import Router from './router'
 
 const app = dva({
   initialState: {},
+  extraEnhancers: [autoRehydrate()],
   onError(e) {
     console.log('onError', e)
   },
@@ -14,5 +16,8 @@ const app = dva({
 registerModels(app)
 app.router(() => <Router />)
 const App = app.start()
+
+// eslint-disable-next-line no-underscore-dangle
+persistStore(app._store, { storage: AsyncStorage })
 
 AppRegistry.registerComponent('DvaStarter', () => App)
