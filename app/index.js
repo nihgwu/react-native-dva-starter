32ad8dddc40ed36c4381729e9ac6a49ede58da68
Neo <nihgwu@live.com>
2018-03-26 15:40:17 +0800

    upgrade deps

--- app/index.js
+++ app/index.js
@@ -7,12 +7,6 @@ import Router, { routerMiddleware } from './router'
 import appModel from './models/app'
 import routerModel from './models/router'
 
-console.ignoredYellowBox = [
-  'Warning: componentWillMount is deprecated',
-  'Warning: componentWillReceiveProps is deprecated',
-  'Warning: componentWillUpdate is deprecated',
-]
-
 const app = dva({
   initialState: {},
   models: [appModel, routerModel],
